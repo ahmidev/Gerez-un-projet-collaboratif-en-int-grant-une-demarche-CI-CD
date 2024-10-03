19a97ee15015ece0e5a9fe5ba420e1cb5e791355
ahmidev <ahmid.aitouali@gmail.com>
2024-10-03 09:23:15 +0200

    coverage test

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,8 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { JokesService } from './services/jokes.service';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { of } from 'rxjs';
-import { Joke } from './model/joke.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -27,37 +25,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should call getRandomJoke on initialization', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    spyOn(app, 'getRandomJoke');
-    
-    app.ngOnInit();
-    
-    expect(app.getRandomJoke).toHaveBeenCalled();
-  });
-
-  it('should call JokesService getRandomJoke when getRandomJoke is called', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    spyOn(jokesService, 'getRandomJoke');
-    
-    app.getRandomJoke();
-    
-    expect(jokesService.getRandomJoke).toHaveBeenCalled();
-  });
-
-  it('should have joke$ as an Observable of Joke from the JokesService', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const mockJoke: Joke = { joke: 'This is a test joke', response: 'Test response' };
-    
-    spyOn(jokesService, 'joke$').and.returnValue(of(mockJoke));
-    
-    app.joke$ = jokesService.joke$();
-    
-    app.joke$.subscribe(joke => {
-      expect(joke).toEqual(mockJoke);
-    });
-  });
 });
--- front/src/app/services/jokes.service.spec.ts
+++ front/src/app/services/jokes.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { JokesService } from './jokes.service';
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
-import { Joke } from '../model/joke.model';
 
 describe('JokesService', () => {
   let service: JokesService;
@@ -23,30 +22,4 @@ describe('JokesService', () => {
     httpMock.verify(); 
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
-  it('should call getRandomJoke and update the subject', () => {
-    service.getRandomJoke();
-
-    const mockJoke: Joke = { joke: 'This is a test joke', response: 'Test response' };
-    const req = httpMock.expectOne('api/joke'); 
-    expect(req.request.method).toBe('GET');
-    req.flush(mockJoke);
-
-    service.joke$().subscribe(joke => {
-      expect(joke).toEqual(mockJoke);
-    });
-  });
-
-  it('should return an observable of Joke from joke$()', () => {
-    const mockJoke: Joke = { joke: 'This is a test joke', response: 'Test response' };
-
-    service['subject'].next(mockJoke);
-
-    service.joke$().subscribe(joke => {
-      expect(joke).toEqual(mockJoke);
-    });
-  });
 });
